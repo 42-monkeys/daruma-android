7bfadc88881e328b03fc155867db59dbd5f77846
baldarn <lorenzo@farnararo.it>
2023-05-05 07:27:05 +0200

    working on notifications

--- app/src/main/java/eu/the42monkeys/MainActivity.kt
+++ app/src/main/java/eu/the42monkeys/MainActivity.kt
@@ -1,7 +1,5 @@
 package eu.the42monkeys
 
-import android.content.pm.PackageManager
-import android.os.Build
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.WindowCompat
@@ -10,8 +8,6 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
-import androidx.activity.result.contract.ActivityResultContracts
-import androidx.core.content.ContextCompat
 import androidx.navigation.findNavController
 import eu.the42monkeys.databinding.ActivityMainBinding
 
@@ -60,33 +56,4 @@ class MainActivity : AppCompatActivity() {
                 || super.onSupportNavigateUp()
     }
 
-    // Declare the launcher at the top of your Activity/Fragment:
-    private val requestPermissionLauncher = registerForActivityResult(
-        ActivityResultContracts.RequestPermission(),
-    ) { isGranted: Boolean ->
-        if (isGranted) {
-            // FCM SDK (and your app) can post notifications.
-        } else {
-            // TODO: Inform user that that your app will not show notifications.
-        }
-    }
-
-    private fun askNotificationPermission() {
-        // This is only necessary for API level >= 33 (TIRAMISU)
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-            if (ContextCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS) ==
-                PackageManager.PERMISSION_GRANTED
-            ) {
-                // FCM SDK (and your app) can post notifications.
-            } else if (shouldShowRequestPermissionRationale(Manifest.permission.POST_NOTIFICATIONS)) {
-                // TODO: display an educational UI explaining to the user the features that will be enabled
-                //       by them granting the POST_NOTIFICATION permission. This UI should provide the user
-                //       "OK" and "No thanks" buttons. If the user selects "OK," directly request the permission.
-                //       If the user selects "No thanks," allow the user to continue without notifications.
-            } else {
-                // Directly ask for the permission
-                requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
-            }
-        }
-    }
 }
--- app/src/main/java/eu/the42monkeys/NotificationService.kt
+++ app/src/main/java/eu/the42monkeys/NotificationService.kt
@@ -1,12 +1,92 @@
 package eu.the42monkeys
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
 import android.app.Service
+import android.content.Context
 import android.content.Intent
+import android.graphics.BitmapFactory
+import android.graphics.Color
+import android.media.RingtoneManager
+import android.os.Build
 import android.os.IBinder
+import android.util.Log
+import androidx.annotation.RequiresApi
+import androidx.core.app.NotificationCompat
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+import kotlin.random.Random
 
-class NotificationService : Service() {
+class NotificationService : FirebaseMessagingService() {
 
-    override fun onBind(intent: Intent): IBinder {
-        TODO("Return the communication channel to the service.")
+    private val DEFAULT_CHANNEL_ID = "default_channel"
+
+    /**
+     * Called if InstanceID token is updated. This may occur if the security of
+     * the previous token had been compromised. Note that this is called when the InstanceID token
+     * is initially generated so this is where you would retrieve the token.
+     */
+    override fun onNewToken(token: String) {
+        super.onNewToken(token)
+        Log.i("NotificationService ", "Refreshed token :: $token")
+        // If you want to send messages to this application instance or
+        // manage this apps subscriptions on the server side, send the
+        // Instance ID token to your app server.
+        sendRegistrationToServer(token)
+    }
+
+    private fun sendRegistrationToServer(token: String) {
+        // TODO : send token to tour server
+    }
+
+    override fun onMessageReceived(message: RemoteMessage) {
+        super.onMessageReceived(message)
+        Log.i("NotificationService ", "Message :: $message")
+
+        val intent = Intent(this, MainActivity::class.java)
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        val notificationID = Random.nextInt(3000)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            setupChannels(notificationManager)
+        }
+
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        val pendingIntent = PendingIntent.getActivity(
+            this, 0, intent,
+            PendingIntent.FLAG_ONE_SHOT or PendingIntent.FLAG_IMMUTABLE
+        )
+
+        val largeIcon = BitmapFactory.decodeResource(
+            resources,
+            R.mipmap.ic_launcher
+        )
+
+        val notificationBuilder = NotificationCompat.Builder(this, DEFAULT_CHANNEL_ID)
+            .setSmallIcon(R.mipmap.ic_launcher)
+            .setLargeIcon(largeIcon)
+            .setContentTitle(message?.data?.get("title"))
+            .setContentText(message?.data?.get("message"))
+            .setAutoCancel(true)
+            .setContentIntent(pendingIntent)
+
+        //Set notification color to match your app color template
+//        notificationBuilder.color = resources.getColor(R.color.background_dark)
+        notificationManager.notify(notificationID, notificationBuilder.build())
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private fun setupChannels(notificationManager: NotificationManager?) {
+        val defaultChannelName = "New notification"
+        val defaultChannelDescription = "daruma reminder"
+
+        val defaultChannel: NotificationChannel =
+            NotificationChannel(DEFAULT_CHANNEL_ID, defaultChannelName, NotificationManager.IMPORTANCE_HIGH)
+        defaultChannel.description = defaultChannelDescription
+        defaultChannel.enableLights(true)
+        defaultChannel.lightColor = Color.RED
+        defaultChannel.enableVibration(true)
+        notificationManager?.createNotificationChannel(defaultChannel)
     }
 }
